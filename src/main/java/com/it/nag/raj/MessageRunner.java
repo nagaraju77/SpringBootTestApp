126558a1045579aeea840af14750a3003f0d3c27
Lenovo <Lenovo@192.168.5.5>
2021-01-10 06:30:02 +0530

    WH-8527-First commit

---
+++ src/main/java/com/it/nag/raj/MessageRunner.java
@@ -0,0 +1,13 @@
+package com.it.nag.raj;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+@Component
+public class MessageRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Welcome to spring boot app");
+	}
+
+}
